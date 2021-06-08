bbb4e1b2575f70a80d3bc6b48d477c62207d19f3
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-06-08 21:35:40 +0300

    1. Бот [#7921] Рефакторинг кода

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,19 +13,20 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    while (!(str = in.readLine()).isEmpty()) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.flush();
+                    while (in.ready() && (str = in.readLine()) != null && !str.isEmpty()) {
                         System.out.println(str);
                         if (str.startsWith("GET")) {
                             String parameter = str.split(" ")[1].split("=")[1];
                             if (parameter.equals("Bye") || parameter.equals("Exit")) {
-                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                                 server.close();
                             } else if (parameter.equals("Hello")) {
-                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                                 out.write("Hello, dear friend.\n".getBytes());
+                                out.flush();
                             } else {
-                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                                 out.write(String.format("%s%n", parameter).getBytes());
+                                out.flush();
                             }
                         }
                     }
