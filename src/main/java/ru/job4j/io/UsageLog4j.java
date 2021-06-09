281fc0ade89b07402b1ad9a04c4712b01fc4addb
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-06-09 22:41:18 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -6,11 +6,23 @@ import org.slf4j.LoggerFactory;
 public class UsageLog4j {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int count = 15;
+        long longCount = 6L;
+        byte byteCount = 1;
+        short shortCount = 124;
+        float floatCount = 4.2F;
+        double doubleCount = 5.7D;
+        char charVariable = 'a';
+        boolean bool = true;
+        LOG.info("Primitive variable 1:{}", count);
+        LOG.info("Primitive variable 2:{}", longCount);
+        LOG.info("Primitive variable 3:{}", byteCount);
+        LOG.info("Primitive variable 4:{}", shortCount);
+        LOG.info("Primitive variable 5:{}", floatCount);
+        LOG.info("Primitive variable 6:{}", doubleCount);
+        LOG.info("Primitive variable 7:{}", charVariable);
+        LOG.info("Primitive variable 8:{}", bool);
     }
 }
