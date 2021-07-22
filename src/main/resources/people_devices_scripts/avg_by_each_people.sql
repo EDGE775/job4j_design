5f4e45d4d4a01b5dd1270bc3ffd90bdc9755db6d
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-07-22 11:43:43 +0300

    2. Группировка и агрегатные функции [#373618 #179783] рефакторинг кода

--- src/main/resources/people_devices_scripts/avg_by_each_people.sql
+++ src/main/resources/people_devices_scripts/avg_by_each_people.sql
@@ -1,4 +1,6 @@
-select dp.people_id, avg(d.price) from devices_people as dp join devices as d
-on dp.device_id = d.id
-group by dp.people_id
+select p.name, avg(d.price)
+from devices_people as dp
+	join devices as d on dp.device_id = d.id
+	join people as p on dp.people_id = p.id
+group by p.name
 having avg(d.price) > 60000;
